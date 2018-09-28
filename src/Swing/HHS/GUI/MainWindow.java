36686a818d63cf568f58b578fcf3f5ca5c7f41fa
Swagpirate <A.muhic@hotmail.com>
2018-09-28 12:59:23 +0200

    Initial layout for start page

--- src/Swing/HHS/GUI/MainWindow.java
+++ src/Swing/HHS/GUI/MainWindow.java
@@ -6,13 +6,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Button;
+import javax.swing.SpringLayout;
+import javax.swing.BoxLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class MainWindow extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	/**
@@ -33,14 +44,49 @@ public class MainWindow extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * @return 
 	 */
 	public MainWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 928, 558);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.CYAN);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Lingo");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.CYAN);
+		contentPane.add(panel, BorderLayout.SOUTH);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{900, 0};
+		gbl_panel.rowHeights = new int[]{50, 50, 0};
+		gbl_panel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JButton btnStart = new JButton("Start");
+		btnStart.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnStart.setHorizontalTextPosition(SwingConstants.CENTER);
+		GridBagConstraints gbc_btnStart = new GridBagConstraints();
+		gbc_btnStart.fill = GridBagConstraints.NONE;
+		gbc_btnStart.insets = new Insets(0, 0, 5, 0);
+		gbc_btnStart.gridx = 0;
+		gbc_btnStart.gridy = 0;
+		panel.add(btnStart, gbc_btnStart);
+		
+		JButton btnCredits = new JButton("Credits");
+		btnCredits.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnCredits.setAlignmentX(Component.CENTER_ALIGNMENT);
+		GridBagConstraints gbc_btnCredits = new GridBagConstraints();
+		gbc_btnCredits.fill = GridBagConstraints.NONE;
+		gbc_btnCredits.gridx = 0;
+		gbc_btnCredits.gridy = 1;
+		panel.add(btnCredits, gbc_btnCredits);
 	}
-
 }
